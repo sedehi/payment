ade5f63b21a209c0601bba6169cf1b7adb5bc1fe
Navid Sedehi <navid.sedehi@gmail.com>
2015-12-07 23:53:23 +0330

    add pasargad exception class

---
+++ src/Sedehi/Payment/Pasargad/PasargadException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Navid Sedehi
+ * Date: 6/1/2015
+ * Time: 11:14 PM
+ */
+
+namespace Sedehi\Payment\Pasargad;
+
+use Sedehi\Payment\PaymentException;
+
+class PasargadException extends PaymentException
+{
+
+    public static $errors = [
+
+    ];
+
+    public function __construct($errorId)
+    {
+        parent::__construct(@self::$errors[$errorId], $errorId);
+    }
+
+}
