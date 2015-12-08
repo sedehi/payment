ffbfe7947382333e32fc1ee87a9d1095d5493e5a
Majid Ghaemifar <mgh145@gmail.com>
2015-12-08 22:31:20 +0330

    Done Payline Request

--- src/Sedehi/Payment/PayLine/Payline.php
+++ src/Sedehi/Payment/PayLine/Payline.php
@@ -8,6 +8,7 @@
 
 namespace Sedehi\Payment\Payline;
 
+use Redirect;
 use Sedehi\Payment\Payment;
 use Sedehi\Payment\PaymentAbstract;
 use Sedehi\Payment\PaymentInterface;
@@ -38,29 +39,26 @@ class Payline extends PaymentAbstract implements PaymentInterface
     {
         $this->newTransaction();
 
-        $callBackUrl = $this->buildQuery($this->callBackUrl, ['trans_id' => $this->transaction->id]);
+        $callBackUrl = $this->buildQuery($this->callBackUrl, ['transaction_id' => $this->transaction->id]);
 
-        //return \PaylineRequest::send($this->requestUrl , $this->api , $this->amount , $this->callBackUrl);
+        //dd($callBackUrl);
 
-        dd('ok');
+        $responseOne = $this->send($this->requestUrl , $this->api , $this->amount , urlencode($this->callBackUrl));
 
-        function send($url,$api,$amount,$redirect){
-            $ch = curl_init();
-            curl_setopt($ch,CURLOPT_URL,$url);
-            curl_setopt($ch,CURLOPT_POSTFIELDS,"api=$api&amount=$amount&redirect=$redirect");
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-            $res = curl_exec($ch);
-            curl_close($ch);
-            return $res;
+        if($responseOne > 0 && is_numeric($responseOne)){
+
+            $this->authority = $responseOne;
+            $this->transactionSetAuthority();
+            $go = $this->secondRequestUrl . $responseOne;
+            return Redirect::to($go);
+
+        }else{
+
+            $this->newLog($this->transaction->id, $responseOne, PaylineException::$errors['send'][$responseOne]);
+            throw new PaylineException('send',$responseOne);
+
         }
-
-        $a = send($this->requestUrl , $this->api , $this->amount , $this->callBackUrl);
-
-        dd($a);
-
-        $response = $soap->bpPayRequest($fields);
-
+/*
         $response = explode(',', $response->return);
         if ($response[0] == 0) {
             $this->authority = $response[1];
@@ -71,11 +69,12 @@ class Payline extends PaymentAbstract implements PaymentInterface
         $this->newLog($this->transaction->id, $response[0], MellatException::$errors[$response[0]]);
 
         throw new MellatException($response[0]);
+*/
     }
 
     public function verify($transaction)
-    {
-        $this->authority  = Input::get('RefId');
+    {dd($transaction);
+        $this->authority  = Input::get('id_get');
         $this->reference  = Input::get('SaleReferenceId');
         $this->cardNumber = Input::get('CardHolderPan');
         if (Input::get('ResCode') == '0') {
@@ -100,6 +99,31 @@ class Payline extends PaymentAbstract implements PaymentInterface
         }
         $this->newLog($this->transaction->id, Input::get('ResCode'), MellatException::$errors[Input::get('ResCode')]);
         throw new MellatException(Input::get('ResCode'));
+    }
+
+
+    private function send($url,$api,$amount,$redirect)
+    {
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,"api=$api&amount=$amount&redirect=$redirect");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return $res;
+    }
+
+    private function get($url,$api,$trans_id,$id_get)
+    {
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,"api=$api&id_get=$id_get&trans_id=$trans_id");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return $res;
     }
 
     public function reversal()
