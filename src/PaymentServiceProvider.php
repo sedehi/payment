99cfe6adcd8b5207886908e03641b48e46ff0600
Navid Sedehi <navid.sedehi@gmail.com>
2017-10-01 21:04:45 +0330

    Update PaymentServiceProvider.php

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -10,7 +10,7 @@ class PaymentServiceProvider extends ServiceProvider
      * Indicates if loading of the provider is deferred.
      * @var bool
      */
-    protected $defer = true;
+    protected $defer = false;
 
     /**
      * Register the service provider.
