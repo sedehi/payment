1a17a1222d7a998000a63dbfca24f7b252dfbf81
Navid Sedehi <navid.sedehi@gmail.com>
2019-01-14 00:51:00 +0330

    models

---
+++ src/Models/Log.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sedehi\Payment\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+
+    public $table = 'transaction_logs';
+
+}
---
+++ src/Models/Transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sedehi\Payment\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+
+    public $table = 'transactions';
+
+}
