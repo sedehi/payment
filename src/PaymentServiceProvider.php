5835407fe0a72375c37014fb1ec951c43a9a1f04
Navid Sedehi <navid.sedehi@gmail.com>
2019-01-13 23:15:21 +0330

    migrations move to database folder

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -6,6 +6,7 @@ use Sedehi\Payment\Commands\ClearUnsuccessfulTransactionsCommand;
 
 class PaymentServiceProvider extends ServiceProvider
 {
+
     /**
      * Indicates if loading of the provider is deferred.
      * @var bool
@@ -16,29 +17,29 @@ class PaymentServiceProvider extends ServiceProvider
      * Register the service provider.
      * @return void
      */
-    public function boot()
-    {
-        $this->publishes([__DIR__.'/config/payment.php' => config_path('payment.php')], 'config');
-        $this->publishes([__DIR__.'/migrations/' => database_path('migrations')], 'migrations');
-        if(file_exists(__DIR__.'/helper.php')){
-            require_once __DIR__.'/helper.php';
-        }
+    public function boot(){
+
+        $this->publishes([__DIR__.'/config/payment.php' => config_path('payment.php')], 'payment-config');
+        $this->publishes([__DIR__.'/database/migrations/' => database_path('migrations')], 'payment-migrations');
     }
 
-    public function register()
-    {
+    public function register(){
+
         $this->mergeConfigFrom(__DIR__.'/config/payment.php', 'payment');
         $this->app->bind('sedehi::command.clear.logs', function($app){
+
             return new ClearLogCommand();
         });
         $this->app->bind('sedehi::command.clear.unsuccessful.transactions', function($app){
+
             return new ClearUnsuccessfulTransactionsCommand();
         });
         $this->commands([
                             'sedehi::command.clear.logs',
                             'sedehi::command.clear.unsuccessful.transactions',
                         ]);
-       $this->app->singleton('payment',function($app){
+        $this->app->singleton('payment', function($app){
+
             return new Payment();
         });
     }
@@ -47,8 +48,8 @@ class PaymentServiceProvider extends ServiceProvider
      * Get the services provided by the provider.
      * @return array
      */
-    public function provides()
-    {
+    public function provides(){
+
         return [];
     }
 }
--- src/database/migrations/2019_01_01_000001_create_currencies_table.php
+++ src/database/migrations/2019_01_01_000001_create_currencies_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGatewaysTable extends Migration
+class CreateCurrenciesTable extends Migration
 {
 
     /**
